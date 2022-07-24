df7d3f343c22433697d06e45506e0b60c5d139c9
Yechiel Worenklein <yechielworen@gmail.com>
2022-07-24 23:28:48 +0300

    It works!

---
+++ server/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title><?php echo $_SERVER['SERVER_ADDR'] ?></title>
+</head>
+  <body>
+<?php
+echo "<h1> The current server is: " . $_SERVER['SERVER_ADDR'] . "</h1>";
+?>
+  <body>
+</html>
